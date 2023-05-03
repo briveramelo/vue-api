995405b2278bbfedd3cf8d7fa775c737c08bbc28
Brandon Rivera-Melo <brandon.rivera-melo@hsc.utah.edu>
2023-05-03 14:52:11 -0500

    establish postgres db connection with docker compose, makefile for easy launch, simplified json handling

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"vue-api/internal/data"
+	"vue-api/internal/driver"
 )
 
 type config struct {
@@ -16,6 +17,7 @@ type application struct {
 	config   config
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	models   data.Models
 }
 
 func main() {
@@ -24,37 +26,33 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	dsn := os.Getenv("DSN")
+	db, err := driver.ConnectPostgres(dsn)
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+	defer db.SQL.Close()
+
 	app := &application{
 		config:   cfg,
 		infoLog:  infoLog,
 		errorLog: errorLog,
+		models:   data.New(db.SQL),
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func (app *application) serve() error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var payload struct {
-			Okay    bool   `json:"okay"`
-			Message string `json:"message"`
-		}
-		payload.Okay = true
-		payload.Message = "Hello, world"
-
-		out, err := json.MarshalIndent(payload, "", "\t")
-		if err != nil {
-			app.errorLog.Println(err)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(out)
-	})
 
 	app.infoLog.Println("API Listening on port", app.config.port)
-	return http.ListenAndServe(fmt.Sprintf(":%d", app.config.port), nil)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
+	}
+	return srv.ListenAndServe()
 }
